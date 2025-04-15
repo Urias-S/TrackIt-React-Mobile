0621bb14c96a93b4c3093d1f1fa43c0c736eecff
Vinicius <viniciusmoura761@gmail.com>
2025-04-15 17:42:15 -0300

    feat: add static sign-up page

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -1,7 +1,99 @@
-export default function Cadastro () {
+import GlobalStyle from "../styles/GlobalStyle";
+import styled from "styled-components";
+import { Link } from "react-router";
+import logo from '../assets/images/Logo.svg'
+export default function Cadastro() {
   return (
     <>
-      Cadastro
+      <GlobalStyle />
+      <Logo>
+        <img src={logo} />
+      </Logo>
+      <Form>
+        <Email type="email" placeholder="email"></Email>
+        <Senha type="password" placeholder="senha"></Senha>
+        <Nome type="text" placeholder="nome"></Nome>
+        <Foto type="text" placeholder="foto"></Foto>
+        <Entrar>Cadastrar</Entrar>
+      </Form>
+      <Login to="/">Já tem uma conta? Faça login!</Login>
     </>
   );
 }
+
+const Login = styled(Link)`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  font-family: "Lexend Deca", sans-serif;
+  color: rgba(82, 182, 255, 1);
+`
+
+const Entrar = styled.button`
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0px;
+  color: white;
+  font-size: 20px;
+  font-weight: 400;
+  background-color: rgba(82, 182, 255, 1);
+  border-radius: 5px;
+  border: none;
+`;
+const Email = styled.input`
+  height: 48px;
+  font-size: 20px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+const Senha = styled.input`
+  font-size: 20px;
+  height: 48px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+
+const Nome = styled.input`
+  font-size: 20px;
+  height: 48px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+const Foto = styled.input`
+  font-size: 20px;
+  height: 48px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  gap: 8px;
+`;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 120px;
+  
+`;
--- src/Pages/Habitos.jsx
+++ src/Pages/Habitos.jsx
@@ -1,7 +1,34 @@
-export default function Habitos () {
+import GlobalStyle from "../styles/GlobalStyle";
+import styled from "styled-components";
+import { Link } from "react-router";
+export default function Habitos() {
   return (
     <>
-      Habitos
+      <GlobalStyle />
+      <Header>
+        <h1>TrackIt</h1>
+        <UserImg src = 'https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg'/>
+      </Header>
     </>
   );
 }
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgba(18, 107, 165, 1);
+  width: 100vw;
+  padding: 10px 20px;
+  h1 {
+    font-family: "Playball", cursive;
+    font-weight: 400;
+    color: white;
+    font-size: 45px;
+  }
+`
+const UserImg = styled.img`
+  height: 55px;
+  width: 55px;
+  border-radius: 100%;
+`
+  
