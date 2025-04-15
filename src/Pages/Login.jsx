dc9c92931ee523429c7ceb5d624a4cd57f39a2a2
Vinicius <viniciusmoura761@gmail.com>
2025-04-15 17:23:18 -0300

    feat: add static login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,75 @@
-export default function Login () {
+import styled from "styled-components";
+import GlobalStyle from "../styles/GlobalStyle";
+import logo from '../assets/images/Logo.svg'
+import { Link } from "react-router";
+export default function Login() {
   return (
     <>
-      login
+      <GlobalStyle />
+      <Logo>
+        <img src={logo} />
+      </Logo>
+      <Form>
+        <Email type="email" placeholder="email"></Email>
+        <Senha type="password" placeholder="senha"></Senha>
+        <Entrar>Entrar</Entrar>
+      </Form>
+      <Cadastro to="/cadastro">Não tem uma conta? Cadastre-se!</Cadastro>
     </>
   );
 }
+const Cadastro = styled(Link)`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  font-family: "Lexend Deca", sans-serif;
+  color: rgba(82, 182, 255, 1);
+`
+
+const Entrar = styled.button`
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0px;
+  color: white;
+  font-size: 20px;
+  font-weight: 400;
+  background-color: rgba(82, 182, 255, 1);
+  border-radius: 5px;
+  border: none;
+`;
+const Email = styled.input`
+  height: 48px;
+  font-size: 20px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+const Senha = styled.input`
+  font-size: 20px;
+  height: 48px;
+  border-radius: 5px;
+  border: solid 1px rgba(212, 212, 212, 1);
+  padding-left: 8px;
+  &::placeholder{
+    color: rgba(219, 219, 219, 1);
+  }
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  gap: 8px;
+`;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 120px;
+  
+`;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap');
+  @media (max-width: 768px) {
+    * {
+      box-sizing: border-box;
+      font-family: "Lexend Deca", sans-serif;
+
+    }
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+`;
+
+export default GlobalStyle;
