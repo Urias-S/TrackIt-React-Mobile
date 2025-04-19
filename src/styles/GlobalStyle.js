b1dcb31f5eca602ac55bd555b5ade9b711f14c40
Vinicius <viniciusmoura761@gmail.com>
2025-04-19 12:29:55 -0300

    fix: remove @import from global style and linked the font on the index

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Playball&display=swap');
   @media (max-width: 768px) {
     * {
       box-sizing: border-box;
